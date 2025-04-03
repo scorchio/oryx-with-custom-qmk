c729da8ea3bee6345a50656a572729d944d52cfa
Zoltán Adamek <djscorchio@gmail.com>
2025-04-03 22:34:44 +0200

    First MIDI attempt

--- qvgLy/keymap.c
+++ qvgLy/keymap.c
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRANSPARENT, KC_LEFT_SHIFT,                                  KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [6] = LAYOUT_voyager(
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-                                                    TO(0),          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
+    MI_Fs1,         MI_G1,          MI_Gs1,         MI_A1,          MI_As1,         MI_B1,                                          MI_Fs3,         MI_G3,          MI_Gs3,         MI_A3,          MI_As3,         MI_B3,          
+    MI_C1,          MI_Cs1,         MI_D1,          MI_Ds1,         MI_E1,          MI_F1,                                          MI_C3,          MI_Cs3,         MI_D3,          MI_Ds3,         MI_E3,          MI_F3,          
+    MI_Fs,          MI_G,           MI_Gs,          MI_A,           MI_As,          MI_B,                                           MI_Fs2,         MI_G2,          MI_Gs2,         MI_A2,          MI_As2,         MI_B2,          
+    MI_C,           MI_Cs,          MI_D,           MI_Ds,          MI_E,           MI_F,                                           MI_C2,          MI_Cs2,         MI_D2,          MI_Ds2,         MI_E2,          MI_F2,          
+                                                    TO(0),          KC_TRANSPARENT,                                 QK_MIDI_TRANSPOSE_DOWN, QK_MIDI_TRANSPOSE_UP
   ),
 };
 
